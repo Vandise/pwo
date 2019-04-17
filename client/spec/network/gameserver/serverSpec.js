59bb2bf83a9147aaa5d92af8dbbe7e399978c309
Benjamin J Anderson <banderson@mashable.com>
2019-04-17 12:05:39 -0500

    Add update and update others events

--- client/spec/network/gameserver/serverSpec.js
+++ client/spec/network/gameserver/serverSpec.js
@@ -207,5 +207,117 @@ describe('GameServer Middleware', () => {
 
       });
     });
+
+    describe('UPDATE_OTHER_PLAYER', () => {
+      const data = { playerID: 1, position: { x: 100, y: 100 }, velocity: {  x: 0, y:0 } };
+
+      beforeEach(() => {
+        td.replace(game, 'addOtherPlayer', sinon.stub());
+        td.replace(game, 'getOtherPlayer', sinon.stub());
+      });
+
+      afterEach(() => {
+        td.reset();
+      });
+
+      describe('when the player does not exist', () => {
+        it('adds the player to the world', () => {
+          game.getOtherPlayer.returns(null);
+
+          mockMiddleware(store)(() => true)({
+            type: `${id}_*`, payload: {
+              type: events.SERVER.PLAYER.UPDATE_OTHER_PLAYER, data
+            }
+          });
+
+          expect(game.addOtherPlayer).to.have.been.called;
+        });
+      });
+
+      describe('when the player does exist', () => {
+
+        let player;
+
+        beforeEach(() => {
+          player = {
+            move: sinon.spy(),
+            setHeading: sinon.spy(),
+            pos: { x: 0, y: 0 },
+            body: { vel: { x: 1, y: 1 } }
+          };
+
+          game.getOtherPlayer.returns(player);
+        });
+
+        describe('when the update type is move', () => {
+
+          it('calls move on the player', () => {
+            mockMiddleware(store)(() => true)({
+              type: `${id}_*`, payload: {
+                type: events.SERVER.PLAYER.UPDATE_OTHER_PLAYER, data: { type: 'move' }
+              }
+            });
+
+            expect(player.move).to.have.been.called;
+          });
+
+        });
+
+        describe('when the update type is setpos', () => {
+
+          const position = { x: 10, y: 10 };
+          const velocity = { x: 10, y: 10 };
+
+          describe('and its forced', () => {
+
+            it('updates the player', () => {
+              mockMiddleware(store)(() => true)({
+                type: `${id}_*`, payload: {
+                  type: events.SERVER.PLAYER.UPDATE_OTHER_PLAYER, data: { type: 'setpos', position, velocity, force: true }
+                }
+              });
+
+              expect(player.pos.x).to.equal(position.x);
+              expect(player.pos.y).to.equal(position.y);
+              expect(player.body.vel.x).to.equal(velocity.x);
+              expect(player.body.vel.y).to.equal(velocity.y);
+              expect(player.setHeading).to.have.been.called;
+            });
+
+          });
+
+          describe('when the player is moving', () => {
+            it('does not update the player', () => {
+              mockMiddleware(store)(() => true)({
+                type: `${id}_*`, payload: {
+                  type: events.SERVER.PLAYER.UPDATE_OTHER_PLAYER, data: { type: 'setpos', position, velocity, force: false }
+                }
+              });
+              expect(player.setHeading).to.not.have.been.called
+            });
+          });
+
+          describe('when the player is not moving', () => {
+            it('updates the player', () => {
+
+              player.body.vel = { x: 0, y: 0 };
+
+              mockMiddleware(store)(() => true)({
+                type: `${id}_*`, payload: {
+                  type: events.SERVER.PLAYER.UPDATE_OTHER_PLAYER, data: { type: 'setpos', position, velocity, force: false }
+                }
+              });
+
+              expect(player.pos.x).to.equal(position.x);
+              expect(player.pos.y).to.equal(position.y);
+              expect(player.body.vel.x).to.equal(velocity.x);
+              expect(player.body.vel.y).to.equal(velocity.y);
+              expect(player.setHeading).to.have.been.called;
+            });
+          });
+
+        });
+      });
+    });
   });
 });
--- client/src/network/gameserver/server/player.js
+++ client/src/network/gameserver/server/player.js
@@ -3,18 +3,39 @@ import * as Dispatcher from 'Util/dispatcher';
 import * as Constants from 'Root/constants';
 import Game from 'Game/';
 
-export const dispatchUpdatePosition = (action, data, dispatch) => {
-  const player = Game.getMainPlayerEntity();
-  player.pos.x = data.position.x;
-  player.pos.y = data.position.y;
+/*
+  
+     UPDATE_POSITION
+      parameters:
+        position: { x, y }
+        velocity: { x, y }
+        displayNotification: true|false
+  
+*/
 
-  Dispatcher.dispatchAction(
-    Dispatcher.actions.forms.SET_CONTENT('Invalid Movement', 'Your position was invalid and has been updated to the last known valid location.')
-  ).then(() => {
+export const dispatchUpdatePosition = (action, data, dispatch) => {
+
+  const player = Game.getMainPlayerEntity();
+
+  if (!player) { return };
+
+  if(player.body.vel.x == 0 && player.body.vel.y == 0) {
+    player.pos.x = data.position.x;
+    player.pos.y = data.position.y;
+    player.body.vel.x = data.velocity.x;
+    player.body.vel.y = data.velocity.y;
+    player.setHeading(data.velocity.x, data.velocity.y);
+  }
+
+  if (data.displayNotification) {
     Dispatcher.dispatchAction(
-      Dispatcher.actions.forms.TOGGLE_FORM('message', true)
-    );
-  });
+      Dispatcher.actions.forms.SET_CONTENT('Invalid Movement', 'Your position was invalid and has been updated to the last known valid location.')
+    ).then(() => {
+      Dispatcher.dispatchAction(
+        Dispatcher.actions.forms.TOGGLE_FORM('message', true)
+      );
+    });
+  }
 };
 
 const UPDATE_POSITION = {
@@ -22,6 +43,82 @@ const UPDATE_POSITION = {
   dispatch: dispatchUpdatePosition
 };
 
+/*
+  
+     UPDATE_OTHER_PLAYER
+        position: { x, y }
+        velocity: { x, y }
+        type: move|setpos      
+  
+*/
+
+export const dispatchUpdateOtherPlayer = (action, data, dispatch) => {
+  if (Game.readyStatus) {
+    const player = Game.getOtherPlayer(`player_${data.playerID}`);
+    if (player) {
+
+      switch(data.type) {
+
+        case 'move':
+          player.move(data);
+          break;
+
+        case 'setpos':
+
+          //
+          // force update ie speed error
+          //
+          if(data.force == true) {
+
+            //console.log('forced update for: ', player.name, JSON.stringify(player.body.vel));
+
+            player.pos.x = data.position.x;
+            player.pos.y = data.position.y;
+            player.body.vel.x = data.velocity.x;
+            player.body.vel.y = data.velocity.y;
+
+            player.setHeading(data.velocity.x, data.velocity.y);
+          } else {
+
+            //console.log('non-forced update for: ', player.name, JSON.stringify(player.body.vel));
+
+            //
+            // not forceful, so only update players currently not moving
+            // and have yet to register their position with the server
+            //
+            if(player.body.vel.x == 0 && player.body.vel.y == 0) {
+              player.pos.x = data.position.x;
+              player.pos.y = data.position.y;
+              player.body.vel.x = data.velocity.x;
+              player.body.vel.y = data.velocity.y;
+
+              player.setHeading(data.velocity.x, data.velocity.y);
+            }
+          }
+
+          break;
+
+        default:
+          return;
+      }
+    } else {
+      Game.addOtherPlayer(`player_${data.playerID}`, data);
+    }
+  }
+};
+
+const UPDATE_OTHER_PLAYER = {
+  action: events.SERVER.PLAYER.UPDATE_OTHER_PLAYER,
+  dispatch: dispatchUpdateOtherPlayer
+};
+
+/*
+  
+    exports
+  
+*/
+
 export default [
-  UPDATE_POSITION
+  UPDATE_POSITION,
+  UPDATE_OTHER_PLAYER
 ];
